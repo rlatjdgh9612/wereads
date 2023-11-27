023633b9f8a6d927932aa004203c1cbc38b569d4
김성호 <rlatjdgh9612@naver.com>
2023-11-27 19:25:50 +0900

    Add. 로그인 UI 구현

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,7 +4,26 @@ import './Login.scss';
 const Login = () => {
   return (
     <div>
-      <h1>로그인 페이지 입니다</h1>
+      <div className="login">
+        <div className="userFrame">
+          <div className="imageFrame">
+            <img className="logo" src="/../images/Logo.svg" alt="위코드 로고" />
+            <img
+              className="logo"
+              src="/../images/logo_wecode.svg"
+              alt="위코드 로고"
+            />
+          </div>
+          <input className="userInput" type="text" placeholder="이메일" />
+          <input className="userInput" type="text" placeholder="비밀번호" />
+          <button className="loginButton">로그인</button>
+          <div className="buttonWrapper">
+            <button className="actionButton">회원 가입</button>
+            <div className="wall">|</div>
+            <button className="actionButton">비밀번호 찾기</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
