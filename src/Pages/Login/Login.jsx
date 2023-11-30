00566eab8be0fac3a54e82009b3dd5fd3dd8f39d
김성호 <rlatjdgh9612@naver.com>
2023-11-30 17:37:54 +0900

    Modify: 리뷰 반영

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,28 +5,26 @@ import './Login.scss';
 
 const Login = () => {
   return (
-    <>
-      <div className="login">
-        <div className="userFrame">
-          <div className="imageFrame">
-            <img className="logo" src="/images/Logo.svg" alt="위코드 로고" />
-            <img
-              className="logo"
-              src="/images/logo_wecode.svg"
-              alt="위코드 로고"
-            />
-          </div>
-          <UserInput type="text" placeholder="이메일" />
-          <UserInput type="password" placeholder="비밀번호" />
-          <UserButton text="로그인" />
-          <div className="buttonWrapper">
-            <button className="actionButton">회원 가입</button>
-            <div className="wall">|</div>
-            <button className="actionButton">비밀번호 찾기</button>
-          </div>
+    <div className="login">
+      <div className="userFrame">
+        <div className="imageFrame">
+          <img className="logo" src="/images/Logo.svg" alt="위코드 로고" />
+          <img
+            className="logo"
+            src="/images/logo_wecode.svg"
+            alt="위코드 로고"
+          />
+        </div>
+        <UserInput type="text" placeholder="이메일" />
+        <UserInput type="password" placeholder="비밀번호" />
+        <UserButton text="로그인" />
+        <div className="buttonWrapper">
+          <button className="actionButton">회원 가입</button>
+          <div className="wall">|</div>
+          <button className="actionButton">비밀번호 찾기</button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
