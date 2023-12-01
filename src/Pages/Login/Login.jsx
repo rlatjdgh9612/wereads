4a93b4c7d4885d990b501b4ca3be9fda9c80caf1
김성호 <rlatjdgh9612@naver.com>
2023-12-01 16:59:43 +0900

    Add. 이메일, 비밀번호 입력 로직 추가, (로그인 버튼 활성화 기능은 구현중)

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import UserInput from '../../Components/UserInput';
 import UserButton from '../../Components/UserButton';
 import './Login.scss';
 
 const Login = () => {
+  // 유저 정보(ID, PW)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // 이메일(ID)
+  const userID = event => {
+    setEmail(event.target.value);
+  };
+  // 비밀번호
+  const userPW = event => {
+    setPassword(event.target.value);
+  };
+  // 유효성 검사
+  const isInvaild =
+    email.includes('@') && email.includes('.') && password.length >= 10;
+
   return (
     <div className="login">
       <div className="userFrame">
@@ -15,9 +31,13 @@ const Login = () => {
             alt="위코드 로고"
           />
         </div>
-        <UserInput type="text" placeholder="이메일" />
-        <UserInput type="password" placeholder="비밀번호" />
-        <UserButton text="로그인" />
+        <UserInput type="text" placeholder="이메일" onChange={userID} />
+        <UserInput type="password" placeholder="비밀번호" onChange={userPW} />
+        <UserButton
+          className="loginButton"
+          text="로그인"
+          // disabled={!isInvaild}
+        />
         <div className="buttonWrapper">
           <button className="actionButton">회원 가입</button>
           <div className="wall">|</div>
