aced0be904407e4317bc43f1818ed5897a02d58a
김성호 <rlatjdgh9612@naver.com>
2023-12-04 14:46:12 +0900

    Fix: 코드리뷰 일부 반영(수정중)

--- src/Pages/SignupComplete/SignupComplete.jsx
+++ src/Pages/SignupComplete/SignupComplete.jsx
@@ -3,18 +3,24 @@ import './SignupComplete.scss';
 
 const SignupComplete = () => {
   return (
-    <div>
-      <div className="signupcomplete">
-        <div className="backButtonFrame">
-          <img className="backIcon" src="/images/Back_arrow.svg"></img>
-          <button className="backButton">뒤로</button>
-        </div>
-        <div className="completeFrame">
-          <img className="completelogo" src="/images/banner_square.svg" />
-          <h1 className="mainText">회원 가입되었습니다!</h1>
-          <p className="subText">이제 로그인해주세요.</p>
-          <button className="completeButton">확인</button>
-        </div>
+    <div className="signupcomplete">
+      <div className="backButtonFrame">
+        <img
+          className="backIcon"
+          src="/images/Back_arrow.svg"
+          alt="뒤로 버튼"
+        />
+        <button className="backButton">뒤로</button>
+      </div>
+      <div className="completeFrame">
+        <img
+          className="completelogo"
+          src="/images/banner_square.svg"
+          alt="완료 로고"
+        />
+        <h1 className="mainText">회원 가입되었습니다!</h1>
+        <p className="subText">이제 로그인해주세요.</p>
+        <button className="completeButton">확인</button>
       </div>
     </div>
   );
