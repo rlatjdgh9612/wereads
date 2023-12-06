625ba258bafbe9ccc29bf17b6d78e9506e4a6881
김성호 <rlatjdgh9612@naver.com>
2023-12-06 19:45:20 +0900

    Fix  로그인 페이지 로직 수정(리뷰 수정중)

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,35 +6,39 @@ import './Login.scss';
 
 const Login = () => {
   // 유저 정보(ID, PW)
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  // 이메일(ID)
-  const userID = event => {
-    setEmail(event.target.value);
-  };
-  // 비밀번호
-  const userPW = event => {
-    setPassword(event.target.value);
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: '',
+  });
+  // 이메일, 비밀번호
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setUserInfo(userInfo => ({
+      ...userInfo,
+      [name]: value,
+    }));
   };
   // 유효성 검사
   const isInvaild =
-    email.includes('@') && email.includes('.') && password.length >= 10;
+    userInfo.email.includes('@') &&
+    userInfo.email.includes('.') &&
+    userInfo.password.length >= 10;
   // 페이지 이동
-  const moveNavigate = useNavigate();
+  const navigate = useNavigate();
   // 회원가입 페이지 이동
-  const goToSignup = () => {
-    moveNavigate('/signup');
+  const goSignupPage = () => {
+    navigate('/signup');
   };
   // fetch : 로그인 버튼 클릭시 메인 페이지로 이동
-  const goToMain = () => {
+  const loginProgcess = () => {
     fetch('/data/Login.json', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
       },
       body: JSON.stringify({
-        email: email,
-        password: password,
+        email: userInfo.email,
+        password: userInfo.password,
       }),
     })
       .then(response => response.json())
@@ -42,7 +46,7 @@ const Login = () => {
         if (data.message === 'LOGIN SUCCESS') {
           alert('로그인 되었습니다.');
           localStorage.setItem('token', data.message);
-          moveNavigate('/Main');
+          navigate('/main');
         } else {
           alert('가입되지 않은 정보입니다.');
         }
@@ -52,7 +56,7 @@ const Login = () => {
 
   return (
     <div className="login">
-      <div className="userFrame">
+      <div className="userFrame" onChange={handleInputChange}>
         <div className="imageFrame">
           <img className="logo" src="/images/Logo.svg" alt="위코드 로고" />
           <img
@@ -65,22 +69,24 @@ const Login = () => {
           className="loginInput"
           type="text"
           placeholder="이메일"
-          onChange={userID}
+          value={userInfo.email}
+          name="email"
         />
         <UserInput
           className="loginInput"
           type="password"
           placeholder="비밀번호"
-          onChange={userPW}
+          value={userInfo.password}
+          name="password"
         />
         <UserButton
           className="loginButton"
           text="로그인"
           disabled={!isInvaild}
-          onClick={goToMain}
+          onClick={loginProgcess}
         />
         <div className="buttonWrapper">
-          <button className="actionButton" onClick={goToSignup}>
+          <button className="actionButton" onClick={goSignupPage}>
             회원 가입
           </button>
           <div className="wall">|</div>
