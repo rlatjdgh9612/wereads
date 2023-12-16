baf12847ae1112e132158012afa38d554968c210
김성호 <rlatjdgh9612@naver.com>
2023-12-17 01:38:35 +0900

    Add. PostEdit 페이지 생성(시간상 관계로 따로 만들었습니다.)

--- src/Pages/PostEdit/PostEdit.jsx
+++ src/Pages/PostEdit/PostEdit.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './PostEdit.scss';
 
 const PostEdit = () => {
+  const userToken = localStorage.getItem('token');
+
+  // 페이지 이동 하기
+  const navigate = useNavigate();
+  const handleCancel = () => {
+    if (window.confirm('수정을 취소하시겠습니까?')) {
+      window.alert('수정이 취소되었습니다.');
+      navigate('/main-thread-list');
+    }
+  };
+
   return (
     <div className="postEdit">
-      <p>여기서 부터는 수정 페이지 입니다</p>
+      <div className="profileWrapper">
+        <div className="profileContainer">
+          <img
+            className="image"
+            src="/images/profileImage_01.jpg"
+            alt="프로필 사진"
+          />
+          <span className="nickName">Name</span>
+        </div>
+        <div className="editInputContainer">
+          <textarea
+            className="editInput"
+            placeholder="내용 수정하기"
+          ></textarea>
+        </div>
+        <div className="editButtonContainer">
+          <button className="editButtons cancelButton" onClick={handleCancel}>
+            취소
+          </button>
+          <button className="editButtons actionButton">게시</button>
+        </div>
+      </div>
     </div>
   );
 };
